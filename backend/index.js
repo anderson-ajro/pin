0c7a79cddc672c8f033f723f609f96ab4f3b2944
Anderson <83408814+anderson-ajro@users.noreply.github.com>
2022-01-15 14:02:44 -0300

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const dotenv = require ('dotenv');
+const app = express();
+const pinRoute = require ('./routes/pins');
+const userRoute = require ('./routes/users');
+
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL, {useNewUrlParser: true})
+    .then(() => {
+        console.log('MongoDB ha sido conectado a API Craic');
+    })
+    .catch((err) => console.log(err));   
+
+    app.use('/api/pins', pinRoute);
+    app.use('/api/users', userRoute);
+
+app.listen(8800, () => {
+    console.log('Craic ha sido conectado al puerto 8800');
+});
+
+
